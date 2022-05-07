84c245e93133bf963691da294cde732a6d949259
Roberto <roberto@3035tech.com>
2022-05-06 23:00:12 -0300

    feat: global css and widgetForm component

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WidgetForm() {
+  return (
+    <div className={"bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg "}>
+      asd
+    </div>
+  );
+}
+
+export default WidgetForm;
