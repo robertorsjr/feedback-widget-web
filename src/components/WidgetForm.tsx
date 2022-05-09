fb1905ed28180ef810bba976fc22fe1bff876510
Roberto <roberto@3035tech.com>
2022-05-09 10:09:25 -0300

    feat: mobile responsive and closeButton

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { CloseButton } from "./index";
 
 function WidgetForm() {
   return (
-    <div className={"bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg "}>
-      asd
+    <div className={"bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto"}>
+     <header>
+       <span className={"text-xl leading-6"}>
+         Deixe seu feedback
+       </span>
+       <CloseButton/>
+     </header>
+
+      <p>content</p>
+
+      <footer className={"text-xs text-neutral-400"}>
+        Feito com ❤ pela <a className={"underline underline-offset-2"} href={"htttps://rocketseat.com.br"}>Rocketseat</a>
+      </footer>
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Widget } from './Widget'
 export { default as WidgetForm } from './WidgetForm'
+export { default as CloseButton } from './CloseButton'
