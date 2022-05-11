a1d90fad9ac212f6dac49db9cc364b65c539983c
Roberto <roberto@3035tech.com>
2022-05-11 15:27:42 -0300

    fix: fixing width and height of button

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -29,7 +29,7 @@ function ScreenshotButton({onScreenshotTook, screenshot }: ScreenshotButtonProps
 		return(
 			<button
 				type={"button"}
-				className={"p-1 w-18 h-18 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"}
+				className={"p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"}
 				style={{
 					backgroundImage: `url(${screenshot})`,
 					backgroundPosition: 'right bottom',
